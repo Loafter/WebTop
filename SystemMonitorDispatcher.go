eb8bacc74e31784b73bbe76e51d1d74c4311224d
Alex <mopper@yandex.ru>
2014-12-31 21:16:33 +0300

    add mem table info into main page, change MemSample -> CPUAverage

--- SystemMonitorDispatcher.go
+++ SystemMonitorDispatcher.go
@@ -5,19 +5,25 @@ import "encoding/json"
 import "sync"
 
 type SystemMonitorResponse struct {
-	CpuSample CPUSample
+	CPUUsage  CPUAverage
 	MemSample MemSample
 }
 
 type SystemMonitorDispatcher struct {
-	accessMutes sync.Mutex
+	accessMutes   sync.Mutex
+	lastCPUSample CPUSample
+}
+
+func (serviceStateDispatcher *SystemMonitorDispatcher) getCPUUsage() CPUAverage {
+	cpuAveragePer := GetCPUAverage(serviceStateDispatcher.lastCPUSample, GetCPUSample())
+	return cpuAveragePer
 }
 
 func (serviceStateDispatcher *SystemMonitorDispatcher) Dispatch(request Request, responseWriter http.ResponseWriter, httpRequest *http.Request) error {
 	//this is service need lock
 	serviceStateDispatcher.accessMutes.Lock()
 	defer serviceStateDispatcher.accessMutes.Unlock()
-	systemInfo := SystemMonitorResponse{CpuSample: GetCPUSample(), MemSample: GetMemSample()}
+	systemInfo := SystemMonitorResponse{CPUUsage: serviceStateDispatcher.getCPUUsage(), MemSample: GetMemSample()}
 	js, err := json.Marshal(systemInfo)
 	if err != nil {
 		http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
